b25dd64b32207beb3402810472ef088b67637b79
Dmitry Bratsun <dmitry.bratsun@gmail.com>
2021-07-20 10:00:32 +0300

    Implement first validation

--- src/hello-world.test.ts
+++
@@ -1,5 +0,0 @@
-import { greet } from './hello-world';
-
-test('greeting', () => {
-  expect(greet('Foo')).toBe('Hello Foo');
-});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export { greet } from './hello-world';
+import { Order } from './classes/classes';
+
+export const validate = (model: any) =>
+  Promise.all<boolean>([validateInner(model)]);
+
+const validateInner = (model: any): Promise<boolean> => {
+  return new Promise<boolean>((resolve, reject) => {
+    try {
+      let order: Order = JSON.parse(model);
+      resolve(true);
+    } catch (error) {
+      resolve(false);
+    }
+  });
+};
